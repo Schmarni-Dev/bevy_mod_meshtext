b95c63b8202c2f196f3d0b0267a1deee623c81fb
Schmarni <marnistromer@gmail.com>
2025-06-03 06:41:43 +0200

    update to bevy 0.16
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -2,15 +2,7 @@ use bevy::prelude::*;
 use bevy_mod_meshtext::{DepthLayout, HorizontalLayout, MeshText, MeshTextPlugin, VerticalLayout};
 fn main() -> AppExit {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            MeshTextPlugin,
-            bevy_flycam::NoCameraPlayerPlugin,
-        ))
-        .insert_resource(bevy_flycam::MovementSettings {
-            sensitivity: 0.00012, // default: 0.00012
-            speed: 2.0,           // default: 12.0
-        })
+        .add_plugins((DefaultPlugins, MeshTextPlugin))
         .add_systems(Startup, setup)
         .add_systems(Update, draw_origin)
         .run()
@@ -41,7 +33,7 @@ fn setup(mut cmds: Commands, mut materials: ResMut<Assets<StandardMaterial>>) {
         },
         VerticalLayout::Centered,
         DepthLayout::Front,
-        HorizontalLayout::Centered
+        HorizontalLayout::Centered,
     ));
     cmds.spawn((
         MeshMaterial3d(text),
@@ -56,7 +48,6 @@ fn setup(mut cmds: Commands, mut materials: ResMut<Assets<StandardMaterial>>) {
     cmds.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.5, 0.5, -2.0).looking_at(Vec3::ZERO, Dir3::Y),
-        bevy_flycam::FlyCam,
     ));
 }
 fn draw_origin(query: Query<&GlobalTransform, With<MeshText>>, mut gizmos: Gizmos) {
